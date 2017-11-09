3f37d1fc2635ebbb29a520181ad6b827c10234dc
Joseph <joekimga@gmail.com>
2017-11-08 21:36:37 -0500

    WIP: after tutor session

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+// var apiRoutes = require("./routing/apiRouts.js");
+// 	/app/routing/apiRouts.js
 
 var app = express();
-var port = 3000;
+const PORT = 3000;
+
+app.get("/", function(req,res) {
+	res.sendfile(path.join(__dirname, '/app/public/home.html'));
+});
+
+app.get("/survey", function(req, res) {
+	res.sendfile(path.join(__dirname, '/app/public/survey.html'));
+})
+
+
+
+
+// app.get("/api", apiRoutes, )  
+
+
+app.listen(PORT, function() {
+	console.log("listening on ", PORT)
+});
